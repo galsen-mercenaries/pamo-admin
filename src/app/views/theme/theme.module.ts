eccffbe59076fda71c9e2601cd924f22019c6cfd
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-11-18 23:20:04 +0000

    Update modal creation user

--- src/app/views/theme/theme.module.ts
+++ src/app/views/theme/theme.module.ts
@@ -1,58 +1,60 @@
 // Angular
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
 
 // Theme Routing
-import { ThemeRoutingModule } from './theme-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AddNewsComponent } from './news/add-news/add-news.component';
-import { AutresComponent } from './autres/autres.component';
-import { UtilisateurComponent } from './utilisateur/utilisateur.component';
-import { NewsComponent } from './news/news.component';
-import { EditUserComponent } from './utilisateur/components/edit-user/edit-user.component';
-import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DeleteItemComponent } from './utilisateur/components/delete-item/delete-item.component';
+import {ThemeRoutingModule} from './theme-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AddNewsComponent} from './news/add-news/add-news.component';
+import {AutresComponent} from './autres/autres.component';
+import {UtilisateurComponent} from './utilisateur/utilisateur.component';
+import {NewsComponent} from './news/news.component';
+import {EditUserComponent} from './utilisateur/components/edit-user/edit-user.component';
+import {MatDialogModule, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {DeleteItemComponent} from './utilisateur/components/delete-item/delete-item.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import { ItemsFormulaireComponent } from './autres/components/items-formulaire/items-formulaire.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { MeetingComponent } from './meeting/meeting.component';
-import { EditMeetingComponent } from './meeting/components/edit-meeting/edit-meeting.component';
-import { DeleteMeetingComponent } from './meeting/components/delete-meeting/delete-meeting.component';
-import { PrestataireComponent } from './prestataire/prestataire.component';
-import { AddPrestataireComponent } from './prestataire/components/add-prestataire/add-prestataire.component';
-import { DeletePrestataireComponent } from './prestataire/components/delete-prestataire/delete-prestataire.component';
-import { EditPrestataireComponent } from './prestataire/components/edit-prestataire/edit-prestataire.component';
+import {ItemsFormulaireComponent} from './autres/components/items-formulaire/items-formulaire.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {MeetingComponent} from './meeting/meeting.component';
+import {EditMeetingComponent} from './meeting/components/edit-meeting/edit-meeting.component';
+import {DeleteMeetingComponent} from './meeting/components/delete-meeting/delete-meeting.component';
+import {PrestataireComponent} from './prestataire/prestataire.component';
+import {AddPrestataireComponent} from './prestataire/components/add-prestataire/add-prestataire.component';
+import {DeletePrestataireComponent} from './prestataire/components/delete-prestataire/delete-prestataire.component';
+import {EditPrestataireComponent} from './prestataire/components/edit-prestataire/edit-prestataire.component';
+import {MatSortModule} from '@angular/material/sort';
 @NgModule({
-  imports: [ 
-    CommonModule,
-    ThemeRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDialogModule,
-    MatCardModule,
-    MatTableModule,
-    MatPaginatorModule,
-    NgxPaginationModule
-  ],
-  entryComponents: [EditUserComponent, DeleteItemComponent, ItemsFormulaireComponent],
-  declarations: [
-    UtilisateurComponent,
-    NewsComponent,
-    AddNewsComponent,
-    AutresComponent,
-    EditUserComponent,
-    DeleteItemComponent,
-    ItemsFormulaireComponent,
-    MeetingComponent,
-    EditMeetingComponent,
-    DeleteMeetingComponent,
-    PrestataireComponent,
-    AddPrestataireComponent,
-    DeletePrestataireComponent,
-    EditPrestataireComponent
-  ],
-  providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
+    imports: [
+        CommonModule,
+        ThemeRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatCardModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        NgxPaginationModule
+    ],
+    entryComponents: [EditUserComponent, DeleteItemComponent, ItemsFormulaireComponent],
+    declarations: [
+        UtilisateurComponent,
+        NewsComponent,
+        AddNewsComponent,
+        AutresComponent,
+        EditUserComponent,
+        DeleteItemComponent,
+        ItemsFormulaireComponent,
+        MeetingComponent,
+        EditMeetingComponent,
+        DeleteMeetingComponent,
+        PrestataireComponent,
+        AddPrestataireComponent,
+        DeletePrestataireComponent,
+        EditPrestataireComponent
+    ],
+    providers: [{provide: MAT_DIALOG_DATA, useValue: {}}]
 })
-export class ThemeModule { }
+export class ThemeModule {}
--- src/app/views/theme/utilisateur/components/edit-user/edit-user.component.ts
+++ src/app/views/theme/utilisateur/components/edit-user/edit-user.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { UserModel } from '../../../../../models/user.model';
+import { AutresService } from '../../../autres/autres.service';
 
 @Component({
   selector: 'app-edit-user',
@@ -10,7 +11,8 @@ import { UserModel } from '../../../../../models/user.model';
 })
 export class EditUserComponent implements OnInit {
   userForm: FormGroup;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {user: UserModel, mode?: 'edit' | 'see' | 'create'}) { }
+  structuresSanitaires$;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {user: UserModel, mode?: 'edit' | 'see' | 'create'}, private autresService: AutresService) { }
 
   ngOnInit(): void {
     this.userForm = new FormGroup ({
@@ -20,13 +22,20 @@ export class EditUserComponent implements OnInit {
       email : new FormControl({value: this.data?.user?.email, disabled: this.data.mode === 'see'}, Validators.required),
       numero : new FormControl({value: this.data?.user?.numero, disabled: this.data.mode === 'see'}, Validators.required),
       adresse : new FormControl({value: this.data?.user?.adresse, disabled: this.data.mode === 'see'}, Validators.required),
-      role : new FormControl({value: this.data?.user?.role?.code ? this.data?.user?.role?.code : 'ROLE_USER', disabled: this.data.mode === 'see'}, Validators.required),
-    })
+      roleCode : new FormControl({value: this.data?.user?.role?.code ? this.data?.user?.role?.code : 'ROLE_USER', disabled: this.data.mode === 'see'}, Validators.required),
+      structuresanitaireId : new FormControl({value: this.data?.user?.structuresanitaireId , disabled: this.data.mode === 'see'}),
+      password : new FormControl(),
+    });
+    this.getStructureSanitaire();
   }
 
   onUpdate() {
     console.log('form', this.userForm.value);
 
+  }
+
+  getStructureSanitaire(){
+    this.structuresSanitaires$ = this.autresService.getStructureSanitaire();
   }
 
 }
