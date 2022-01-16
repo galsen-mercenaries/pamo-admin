70f3388fb9d42b3afb609adc4e6c2ede27938192
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-01-16 15:13:37 +0000

    Add heroku build profile

---
+++ src/environments/environment.heroku.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://pamo-backend-loopback.herokuapp.com',
+    fileMaxSize: 50000000,
+    mapbox: {
+        accessToken: 'pk.eyJ1IjoibWF0ZmFsbCIsImEiOiJja3RsaGx3ODAwMmZpMm5wYmtzNTRrN25kIn0.xjywL0L_vDNlyreSmHOFDA'
+    },
+    style: 'mapbox://styles/mapbox/streets-v11',
+    lat: 14.751523,
+    lng: -17.45677,
+    occurencesList: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+};
