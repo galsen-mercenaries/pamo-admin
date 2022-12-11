e133c2828011eccfb6f4894c1d748412f8249b59
BigBadWolf <mabdoulkarimdiop@gmail.com>
2022-12-11 13:29:05 +0000

    Update environment.heroku.ts

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://pamo-backend-loopback.herokuapp.com/',
+    baseUrl: 'https://pamo-backend-prod.up.railway.app/',
     fileMaxSize: 50000000,
     mapbox: {
         accessToken: 'pk.eyJ1IjoibWF0ZmFsbCIsImEiOiJja3RsaGx3ODAwMmZpMm5wYmtzNTRrN25kIn0.xjywL0L_vDNlyreSmHOFDA'
